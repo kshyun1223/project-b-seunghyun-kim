372ae5c60f7048a93d7264760ece5a4a63df0eff
seunghyunkim <kshyun.dev@gmail.com>
2022-12-14 15:24:13 +0900

    map 컨트롤러에 map 서비스 주입

--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -1,4 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { MapService } from './map.service';
 
 @Controller('map')
-export class MapController {}
+export class MapController {
+  constructor(private mapService : MapService){}
+}
