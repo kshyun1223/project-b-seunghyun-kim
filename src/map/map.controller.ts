a87e53ddf0c1701c03528a016147c04ee79d4591
seunghyunkim <kshyun.dev@gmail.com>
2022-12-14 15:30:17 +0900

    임시로 map 서비스 무력화

--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
-import { MapService } from './map.service';
+// import { MapService } from './map.service';
 
 @Controller('map')
 export class MapController {
-  constructor(private mapService : MapService){}
+  // constructor(private mapService : MapService){}
 }
