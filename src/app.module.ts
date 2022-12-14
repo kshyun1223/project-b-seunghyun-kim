4e228ad5248d77e2942d32729377da2c8a1fd5ea
seunghyunkim <kshyun.dev@gmail.com>
2022-12-14 15:25:09 +0900

    루트 모듈에 map 서비스 프로바이더 주입

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MapModule } from './map/map.module';
+import { MapService } from './map/map.service';
 
 @Module({
-  controllers: [AppController],
   imports: [MapModule],
+  providers: [MapService]
+  // controllers: [AppController],
 })
 export class AppModule {}
