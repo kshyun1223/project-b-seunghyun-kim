2687fcc9f164f185f192afa71b6f665ec2bed8c9
seunghyunkim <kshyun.dev@gmail.com>
2022-12-14 15:31:14 +0900

    임포트 빼먹었네 ㅎㅎ

--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 // import { MapService } from './map.service';
 
 @Controller('map')
 export class MapController {
   // constructor(private mapService : MapService){}
+  @Get()
+  @Render('index')
+  root() {
+    return { message: '젭알좀되라!!!' };
+  }
 }
