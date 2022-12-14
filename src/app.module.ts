3f964085ff73078537b441a11f6e906ba5de7b1e
seunghyunkim <kshyun.dev@gmail.com>
2022-12-14 15:12:07 +0900

    map 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MapModule } from './map/map.module';
 
 @Module({
   controllers: [AppController],
+  imports: [MapModule],
 })
 export class AppModule {}
