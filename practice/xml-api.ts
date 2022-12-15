e6f99bf90902a76230feb5d1b18d48a8b48f1bbf
seunghyun <kshyun.dev@gmail.com>
2022-12-15 22:06:21 +0900

    아오 됐다 ㅠㅠㅠㅠㅠㅠ

--- practice/xml-api.ts
+++ practice/xml-api.ts
@@ -1,13 +1,16 @@
-import express from 'express'
-import xmlparser from 'express-xml-bodyparser';
-
-const app = express()
-app.use(xmlparser());
+import { xml2json } from 'xml-js'
 
 fetch('https://openapi.its.go.kr:9443/trafficInfo?apiKey=test&type=all&minX=126.800000&maxX=127.890000&minY=34.900000&maxY=35.100000&getType=json')
-.then(response => {
-  console.log('Response status: ' + response.status)
-  return response.text()
+.then(res => {
+  console.log('Response status: ' + res.status)
+  const resText = res.text()
+  return resText
 })
-.then(responseText => console.log('Response text: ' + responseText))
+.then(resText => result(resText))
 .catch(error => console.log('Error caught: ' + error))
+
+const result = (xml: string) => {
+  var result1 = xml2json(xml, {compact: true, spaces: 4});
+  var result2 = xml2json(xml, {compact: false, spaces: 4});
+  console.log(result1, '\n', result2)
+}
