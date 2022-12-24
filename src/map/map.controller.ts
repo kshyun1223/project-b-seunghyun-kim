4d2870897f0bc1ab5c72e55b92177556670268ae
seunghyun <kshyun.dev@gmail.com>
2022-12-24 20:24:00 +0900

    템플릿엔진은 html만 제어 가능해서 쓸모없음 + Response로 스크립트까지 제어 성공

--- src/map/map.controller.ts
+++ src/map/map.controller.ts
@@ -1,12 +1,12 @@
-import { Get, Controller, Render } from '@nestjs/common';
+import { Get, Controller, Render, Res } from '@nestjs/common';
+import { response } from 'express';
 import { MapService } from './map.service';
-
+import { Response } from 'express'
 @Controller('/')
 export class MapController {
   constructor(private mapService : MapService){}
   @Get()
-  @Render('index')
-  root() {
-    return this.mapService.getLogic()
+  root(@Res() res: Response) {
+    res.send("<script>alert('test')</script>")
   }
 }
