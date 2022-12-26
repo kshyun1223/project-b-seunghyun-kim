d0b0a3931ff6674974e82aa10958dbad1c5683a4
seunghyunkim <kshyun.dev@gmail.com>
2022-12-26 10:49:05 +0900

    일단 요청은 잘 들어갔는데 xml 파싱을 안해서 그런 거 같다

--- src/traffic/traffic.controller.ts
+++ src/traffic/traffic.controller.ts
@@ -5,10 +5,10 @@ import { Request, Response } from 'express'
 @Controller('traffic')
 export class TrafficController {
   constructor(private trafficService : TrafficService){}
-
   @Get()
   root(@Res() res: Response, @Req() req: Request) {
     console.log(req.query)
-    res.send(JSON.stringify("여기서도 잘되나~"))
+    this.trafficService.getTraffic(req.query.minX, req.query.minY, req.query.maxX, req.query.maxY)
+    res.send(JSON.stringify("더미텍스트"))
   }
 }
--- src/traffic/traffic.service.ts
+++ src/traffic/traffic.service.ts
@@ -5,11 +5,10 @@ import { ConfigService } from '@nestjs/config'
 export class TrafficService {
   constructor(private configService: ConfigService){}
   private key = this.configService.get('TRAFFIC_KEY')
-  getTraffic(){
-    `https://openapi.its.go.kr:9443/trafficInfo?apiKey=${this.key}5e46661666e34f9bbc8e751384787382&type=all&minX=127.3719135&minY=36.33395949&maxX=127.3796322&maxY=36.36467214`
-    /* fetch(`http://localhost:3000/traffic?minX=${minX}&minY=${minY}&maxX=${maxX}&maxY=${maxY}`)
-        .then((response) => response.json())
-        .then((data) => infoWindow.setContent(getContentElement(minX, minY, data))) */
+  getTraffic(minX, minY, maxX, maxY){
+    fetch(`https://openapi.its.go.kr:9443/trafficInfo?apiKey=${this.key}&type=all&minX=${minX}&minY=${minY}&maxX=${maxX}&maxY=${maxY}`)
+      .then((response) => response.json())
+      .then((data) => console.log(data))
   }
 }
 
