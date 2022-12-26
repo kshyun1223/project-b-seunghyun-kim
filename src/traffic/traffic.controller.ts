7f7d747a608b202e57d02c70b1ac84f8ce304452
seunghyunkim <kshyun.dev@gmail.com>
2022-12-26 13:08:50 +0900

    쿼리스트링 검증

--- src/traffic/traffic.controller.ts
+++ src/traffic/traffic.controller.ts
@@ -7,7 +7,7 @@ export class TrafficController {
   constructor(private trafficService : TrafficService){}
   @Get()
   root(@Res() res: Response, @Req() req: Request) {
-    console.log(req.query)
+    console.log(req.query.minX, req.query.minY, req.query.maxX, req.query.maxY)
     this.trafficService.getTraffic(req.query.minX, req.query.minY, req.query.maxX, req.query.maxY)
     res.send(JSON.stringify("더미텍스트"))
   }
