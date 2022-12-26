14c251b7a7bfd4142c3ab9a6365be9aa66930e37
seunghyunkim <kshyun.dev@gmail.com>
2022-12-26 13:08:10 +0900

    테스트 하려고 json 파일 뽑음

--- src/traffic/traffic.service.ts
+++ src/traffic/traffic.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config'
+import * as fs from 'fs'
+const xml2json = require('xml-js')
 
 @Injectable()
 export class TrafficService {
   constructor(private configService: ConfigService){}
   private key = this.configService.get('TRAFFIC_KEY')
-  getTraffic(minX, minY, maxX, maxY){
+  getTraffic(minX, minY, maxX, maxY) {
     fetch(`https://openapi.its.go.kr:9443/trafficInfo?apiKey=${this.key}&type=all&minX=${minX}&minY=${minY}&maxX=${maxX}&maxY=${maxY}`)
-      .then((response) => response.json())
-      .then((data) => console.log(data))
+      .then((res) => {
+        // console.log('Response status: ' + res.status)
+        const rawData = res.text()
+        return rawData
+      })
+      .then((rawData) => {
+        const parsedData = xml2json.xml2json(rawData, {compact: true, spaces: 2})
+        fs.writeFileSync('./test.json', parsedData)
+      })
   }
 }
 
 
 
+// error(4009)
