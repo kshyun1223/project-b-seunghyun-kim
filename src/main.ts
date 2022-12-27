21d739240c8db95decad1095d22dbaa9c83f3ba6
seunghyunkim <kshyun.dev@gmail.com>
2022-12-27 13:46:35 +0900

    NestExpressApplication 모듈 뭐땜에 호출했더라? 지금은 없어도 되네 ㅇㅇ

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { NestFactory } from '@nestjs/core'
-import { NestExpressApplication } from '@nestjs/platform-express'
 import { AppModule } from './app.module'
 import { ConfigService } from "@nestjs/config"
 
 async function main() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule)
+  const app = await NestFactory.create(AppModule)
+  // NestFactory가 뭐지? 코어 모듈이라는건가?
 
   const configService = app.get(ConfigService)
   const port = configService.get('NODE_SERVER_PORT')
