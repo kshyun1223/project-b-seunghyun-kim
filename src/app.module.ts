b8353510eb4fe2bfb592f50db1afecb40465083c
kshyun <kshyun.dev@gmail.com>
2023-01-09 22:16:07 +0900

    환경변수는 app 모듈이 아니라 main.ts에 호출했어야 하는 것 같다

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,8 @@
 import { Module } from '@nestjs/common'
 import { MapModule } from './map/map.module'
-import { ConfigModule } from '@nestjs/config'
 import { TrafficModule } from './traffic/traffic.module';
 
 @Module({
-  imports: [
-    MapModule,
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.prod',
-      ignoreEnvFile: process.env.NODE_ENV === 'prod',
-      }),
-    TrafficModule
-  ],
+  imports: [MapModule, TrafficModule],
 })
 export class AppModule {}
