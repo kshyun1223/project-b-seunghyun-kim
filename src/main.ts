83964c8c1b343042728a739b645da24e264ccfde
kshyun <kshyun.dev@gmail.com>
2023-01-09 21:59:49 +0900

    왜 클라우드에서는 안될까 ㅠㅠ

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,17 @@ async function main() {
   // NestFactory가 뭐지? 코어 모듈이라는건가?
 
   const configService = app.get(ConfigService)
+  const env = configService.get('NODE_ENV')
   const port = configService.get('NODE_SERVER_PORT')
+  const mongoUrl = configService.get('MONGO_URL')
+  const trafficKey = configService.get('TRAFFIC_KEY')
+  const fetchURL = configService.get('FETCH_URL')
 
   await app.listen(port)
+  console.log(`env:${env}`)
   console.log(`port:${port}`)
+  console.log(`mongoUrl:${mongoUrl}`)
+  console.log(`trafficKey:${trafficKey}`)
+  console.log(`fetchURL:${fetchURL}`)
 }
 main()
