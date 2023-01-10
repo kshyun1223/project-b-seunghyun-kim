adaf00a2118dfbf799e740f82c2669535d798387
kshyun <kshyun.dev@gmail.com>
2023-01-10 19:09:01 +0900

    파일시스템 없이 되긴 되는데 컨트롤러에 몰아서 했음. 서비스로 코드 옮겨야 함.

--- src/traffic/traffic.controller.ts
+++ src/traffic/traffic.controller.ts
@@ -2,17 +2,56 @@ import { Get, Controller, Req, Res } from '@nestjs/common';
 import { TrafficService } from './traffic.service';
 import { Request, Response } from 'express'
 import * as fs from 'fs'
+import { rejects } from 'assert';
+const xml2json = require('xml-js')
+import { ConfigService } from '@nestjs/config';
+
 
 @Controller('traffic')
 export class TrafficController {
-  constructor(private trafficService : TrafficService){}
+  constructor(
+    private trafficService : TrafficService,
+    private configService: ConfigService
+  ){}
+  private key = this.configService.get('TRAFFIC_KEY')
   @Get()
   root(@Res() res: Response, @Req() req: Request) {
-    this.trafficService.getTraffic(req.query.minX, req.query.minY, req.query.maxX, req.query.maxY)
+    const getTraffic = (minX, minY, maxX, maxY) => {
+      const url = `https://openapi.its.go.kr:9443/trafficInfo?apiKey=${this.key}&type=all&minX=${minX}&minY=${minY}&maxX=${maxX}&maxY=${maxY}`
+      
+      console.log(url)
+      fetch(url)
+        .then((res => {
+          const rawData = res.text()
+          return rawData
+        }))
+        .then((rawData => {
+          const json = xml2json.xml2json(rawData, {compact: true, spaces: 2})
+          return json
+        }))
+        .then((json => {
+          const parsedData = JSON.parse(json)
+          return parsedData
+        }))
+        .then((parsedData => {
+          const speed = parsedData.response.body.items.item[0].speed._text+'km/h'
+          console.log(speed)
+          // fs.writeFileSync('./src/traffic/temp/speed.txt', speed, 'utf8')
+          send(speed)
+        }))
+        .catch((error => {
+          // fs.writeFileSync('./src/traffic/temp/speed.txt', 'error', 'utf8')
+          console.log("에러 메시지: " + error)
+          send("error")
+        }));
+    }
 
-    const temp = fs.readFileSync('./src/traffic/temp/speed.txt','utf-8')
-    console.log(temp)
+    getTraffic(req.query.minX, req.query.minY, req.query.maxX, req.query.maxY)
 
-    res.send(JSON.stringify(temp))
+    // const temp = fs.readFileSync('./src/traffic/temp/speed.txt','utf-8')
+
+    const send = (speed) => {
+      res.send(JSON.stringify(speed))
+    }
   }
 }
--- src/traffic/traffic.service.ts
+++ src/traffic/traffic.service.ts
@@ -1,34 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config'
 const xml2json = require('xml-js')
-import * as fs from 'fs'
 
 @Injectable()
 export class TrafficService {
-  constructor(private configService: ConfigService){} // 전역으로 설정된 config 모듈은 이렇게 가져온다
+  constructor(private configService: ConfigService){}
   private key = this.configService.get('TRAFFIC_KEY')
-  getTraffic(minX, minY, maxX, maxY) {
+  /* getTraffic(minX, minY, maxX, maxY) {
     const url = `https://openapi.its.go.kr:9443/trafficInfo?apiKey=${this.key}&type=all&minX=${minX}&minY=${minY}&maxX=${maxX}&maxY=${maxY}`
     console.log(url)
     fetch(url)
-      .then((res) => {
+      .then((res => {
         const rawData = res.text()
         return rawData
-      })
-      .then((rawData) => {
+      }))
+      .then((rawData => {
         const json = xml2json.xml2json(rawData, {compact: true, spaces: 2})
         return json
-      })
-      .then((json) => {
+      }))
+      .then((json => {
         const parsedData = JSON.parse(json)
         return parsedData
-      })
-      .then((parsedData) => {
+      }))
+      .then((parsedData => {
         const speed = parsedData.response.body.items.item[0].speed._text+'km/h'
-        fs.writeFileSync('./src/traffic/temp/speed.txt', speed, 'utf8')
-      })
-      .catch((error) => {
-        fs.writeFileSync('./src/traffic/temp/speed.txt', 'error', 'utf8')
-      });
-  }
+        // console.log(speed)
+        // fs.writeFileSync('./src/traffic/temp/speed.txt', speed, 'utf8')
+        return speed
+      }))
+      .catch((error => {
+        // fs.writeFileSync('./src/traffic/temp/speed.txt', 'error', 'utf8')
+        // console.log("에러 메시지: " + error)
+        return "error"
+      }));
+  } */
 }
